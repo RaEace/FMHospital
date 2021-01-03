b72ca15955c611f4c831f8cbccb8f3a4de61e17b
jules <jules.lagny@efrei.net>
2021-01-03 17:24:59 +0100

    few add in Patient & starting methods on EmergencyCareService

--- src/m2/lagny/se/EmergencyCareService.java
+++ src/m2/lagny/se/EmergencyCareService.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.concurrent.Semaphore;
 
 public class EmergencyCareService {
-  private String ServiceName;
+  private String serviceName;
   private Provider provider;
 
   private Semaphore semPhysician;
@@ -13,11 +13,30 @@ public class EmergencyCareService {
 
   private ArrayList<Patient> patients;
   private ArrayList<Patient> patientsNoPaperInWR;
-  private ArrayList<Patient> patientSPaperInWR;
+  private ArrayList<Patient> patientsPaperInWR;
   private ArrayList<Patient> patientsInRoom;
 
-  public void PatientIsAccepted() {
+  public EmergencyCareService(String serviceName, Provider provider) {
+    this.serviceName = serviceName;
+    this.provider = provider;
+    this.semPhysician = new Semaphore(0);
+    this.semRooms = new Semaphore(0);
+    this.semNurses = new Semaphore(0);
+    this.patients = new ArrayList<Patient>();
+    this.patientsNoPaperInWR = new ArrayList<Patient>();
+    this.patientsPaperInWR = new ArrayList<Patient>();
+    this.patientsInRoom = new ArrayList<Patient>();
+  }
 
+  public void PatientIsAccepted(Patient patient) {
+    if (patients.contains(patient) && !patient.getCured()) {
+      System.out.println("(" + serviceName + ") | " + patient + " entered in service and go to waiting-room");
+      this.patientsNoPaperInWR.add(patient);
+    }
+    else {
+      System.out.println("(" + serviceName + ") | " + patient + " canno't join the service");
+    }
+    this.patients.remove(patient);
   }
 
   public void PatientIsWaitingWithoutPaper() {
--- src/m2/lagny/se/Patient.java
+++ src/m2/lagny/se/Patient.java
@@ -3,4 +3,22 @@ package m2.lagny.se;
 public class Patient {
   private String name;
   private boolean cured;
+
+  public Patient(String name, boolean cured) {
+    this.name = name;
+    this.cured = cured;
+  }
+
+  public boolean getCured() {
+    return this.cured;
+  }
+
+  public void setCured() {
+    this.cured = true;
+  }
+
+  @Override
+  public String toString() {
+    return name;
+  }
 }
