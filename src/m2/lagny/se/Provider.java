4a0a1a05015d3415154d65e51a2d74b3cab8f547
jules <jules.lagny@efrei.net>
2021-01-03 18:24:43 +0100

    finish Provider class

--- src/m2/lagny/se/Provider.java
+++ src/m2/lagny/se/Provider.java
@@ -3,22 +3,49 @@ package m2.lagny.se;
 import java.util.concurrent.Semaphore;
 
 public class Provider {
-  private Semaphore rooms;
-  private Semaphore physicians;
+  private Semaphore semRooms;
+  private Semaphore semPhysicians;
+  private Semaphore semLock;
 
-  public void gettingARoom() {
-
+  public Provider() {
+    this.semRooms = new Semaphore(0);
+    this.semPhysicians = new Semaphore(0);
+    this.semLock = new Semaphore(1);
   }
 
-  public void givingARoom() {
-
+  public void gettingARoom(String service) throws InterruptedException {
+    this.semLock.acquire();
+    this.semRooms.release();
+    System.out.println("(Provider) | get a room from service : " + service);
+    this.semLock.release();
   }
 
-  public void gettingAPhysician() {
-
+  public void sendARoom(String service) throws InterruptedException {
+    this.semLock.acquire();
+    if(semRooms.tryAcquire()) {
+      System.out.println("(Provider) | send a room to service : " + service);
+      semLock.release();
+    } else {
+      System.out.println("(Provider) | error : no room available");
+      semLock.release();
+    }
   }
 
-  public void givingAPhysician() {
+  public void getAPhysician(String service) throws InterruptedException {
+    this.semLock.acquire();
+    this.semPhysicians.release();
+    System.out.println("(Provider) | get a physician from service : " + service);
+    this.semLock.release();
+  }
 
+  public void givingAPhysician(String service) throws InterruptedException {
+    this.semLock.acquire();
+    if(semRooms.tryAcquire()) {
+      System.out.println("(Provider) | send a physician to service : " + service);
+      semLock.release();
+    } else {
+      System.out.println("(Provider) | error : no physician available");
+      semLock.release();
+    }
   }
 }
